48ca81e94c9d8e427cf2bf8fb8a8005e28059789
Almudena <almudena.vigara@gmail.com>
2022-08-23 12:38:25 +0200

    fix: route of update

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -111,7 +111,7 @@ module.exports.update = (req, res, next) => {
       return eventdata;
     })
     .then((event) => {
-      res.redirect("/users/list", { event, eventIsOwner });
+      res.render("users/list", { event, eventIsOwner });
     })
     .catch((error) => next(error));
 };
